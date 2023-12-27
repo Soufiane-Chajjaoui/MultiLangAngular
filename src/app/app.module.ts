99237a51883069c9dc967e49244a890a184cc66a
Soufiane-Chajjaoui <schajjaoui@gmail.com>
2023-12-27 03:03:08 +0100

    add function in app.module HttpFactoryLoader for return files from webserver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+
 
 @NgModule({
   declarations: [
@@ -13,9 +17,21 @@ import { NavbarComponent } from './navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader : {
+        provide : TranslateLoader ,
+        useFactory : HttpFactoryLoader ,
+         deps : [ HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+export function HttpFactoryLoader(http:HttpClient) {
+    return new TranslateHttpLoader(http , './assets/i18n/' , '.json');
+}
